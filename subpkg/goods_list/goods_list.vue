43c2a2947616e640eed85df96e521c53f4db9d05
nobody <duanjw@shanghaitech.edu.cn>
2022-03-23 21:44:10 +0800

    goods_list

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
   <view>
-    goodsList
+    <view class="goods-box">
+      <view v-for="(it,i) in goodsList" :key="i" @click="toDetail(it)">
+        <my-goods :item="it"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -8,12 +12,59 @@
   export default {
     data() {
       return {
-        
+        // 编程式导航过来的
+        queryObj: {
+          query: '', // home过来的
+          cid: '', // 分类过来的
+          pagenum: 1,
+          pagesize: 10,
+        },
+        goodsList: [],
+        total: 0,
+        isLoading:false
       };
+    },
+    onLoad(options) {
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoods()
+    },
+    onReachBottom() {
+      if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) {
+        return console.log('获取数据完毕');
+      }
+      if(this.isLoading) return
+      this.queryObj.pagenum += 1
+      this.getGoods()
+    },
+    onPullDownRefresh() {
+      this.queryObj.pagenum = 1
+      this.goodsList = []
+      this.total = 0
+      this.isLoading = false
+      this.getGoods(()=>{ uni.stopPullDownRefresh() })
+    },
+    methods: {
+      async getGoods(cb) {
+        this.isLoading = true
+        let res = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        this.isLoading = false
+        cb && cb()
+        this.goodsList = [...this.goodsList,...res.data.message.goods]
+        this.total = res.data.message.total
+      },
+      toDetail(item){
+        console.log(item);
+        uni.navigateTo({
+          url:"../goods_detail/goods_detail?goods_id=" + item.goods_id
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+  .goods-box {
+    background-color: #FFFFFF;
+  }
 </style>
